b969234c9047edeeceb2e767b23840522d698211
Kok Chee Kean <kok_chee_kean@tech.gov.sg>
2022-10-28 18:28:20 +0800

    update test with mock

--- is-sg-phone-number-valid/src/test/java/com/govtech/commons/MainTest.java
+++ is-sg-phone-number-valid/src/test/java/com/govtech/commons/MainTest.java
@@ -1,15 +1,30 @@
 package com.govtech.commons;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MainTest {
   @Test
-  void test() {
-    var result = new JSONObject();
-    result.put("phoneNumber", JSONObject.NULL);
-    result.put("isValid", true);
-    var responseString = result.toString();
-    System.out.println(responseString);
+  @DisplayName("Given a valid number, return isValid as true and statusCode 200")
+  void givenValidNumberReturnIsValidTrue() {
+    var mockContext = mock(Context.class);
+    var mockLogger = mock(LambdaLogger.class);
+    when(mockContext.getLogger()).thenReturn(mockLogger);
+    var request = new APIGatewayProxyRequestEvent();
+    request.setBody("{\"number\":\"91234567\"}");
+    var app = new Main();
+    var response = app.handleRequest(request, mockContext);
+    assertEquals(200, response.getStatusCode());
+    var responseBody = new JSONObject(response.getBody());
+    assertTrue(responseBody.getBoolean("isValid"));
   }
 }
